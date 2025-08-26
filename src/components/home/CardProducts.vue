03a32bec527ed67d9e133b88cfabbb4563f47e76
karimmahmoud2022 <kelgamal905@gmail.com>
2025-08-26 16:28:23 +0300

    karim

--- src/components/home/CardProducts.vue
+++ src/components/home/CardProducts.vue
@@ -5,9 +5,14 @@
                 <h2>الاكثر مبيعا</h2>
                 <a href="#">مشاهدة الكل</a>
             </div>
-            <div class="row  g-4">
-                 <div class="col-md-5">
-
+            <div class="row  row-cols-1 row-cols-sm-2 row-cols-md-5 g-4">
+                 <div class="col">
+                      <div class="card h-100">
+                        <img src="/src/assets/images/6.webp"/>
+                        <div class="body">
+                             <h5 class="card-title">Rolex Yacht-Master II – rose Gold, Black Dial & Rubber Strap</h5>
+                        </div>
+                      </div>
                  </div>
             </div>
         </div>
